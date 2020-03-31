961d0c871ae80a1c83f21bd734f1638cdcffd2dc
Inigo <inigosoba@gmail.com>
2020-03-31 17:32:34 +0000

    subo proyectos de practicas anteriores

---
+++ simple/src/test/java/106435/AppTest.java
@@ -0,0 +1,27 @@
+package 106435;
+
+import static org.junit.Assert.assertTrue;
+
+IMPORT STATIC JAVA.SCANNER
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void checkString(){
+		Scanner scan = new Scanner();
+		expectedResult = readLine();
+		assertEquals(expectedResult, "Hola Mundo");
+	}
+}
